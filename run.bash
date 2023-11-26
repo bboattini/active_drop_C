068da34c5ae92b80e14f070c997d009f586a8e04
bboattini <b.boattini@gmail.com>
2023-11-26 18:02:09 -0300

    Autosave files into different directories

--- run.bash
+++ run.bash
@@ -22,12 +22,25 @@ for fo in 0.00
 
 for CI in 1 2 
 	do
+# Create the directory
+dir_name="dados_${CI}_L_${L}_R_${R}_a_${a}_h_${h}_w_${w}_fo_${fo}"
+mkdir -p $dir_name
+
+# Move the executable to the directory
+cp gota.out $dir_name
+
+# Change to the directory
+cd $dir_name
+
 
 # ==============================================================================
 # =                               Rodando simulação                            = 
 #===============================================================================
 
-./gota.out -L ${L} -R ${R} -a ${a} -h ${h} -w ${w} -fo ${fo} -CI ${CI} -s  12345567 
+./gota.out -L ${L} -R ${R} -a ${a} -h ${h} -w ${w} -fo ${fo} -CI ${CI} -s  12345567
+
+# Change back to the original directory
+cd -
 
 done
 done
